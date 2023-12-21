315409b2de668618b46921de013560b67afcaa53
KimberlyPangilinan <kimberlypangilinan2001@gmail.com>
2023-12-21 19:22:27 +0800

    feat: added error middleware, added user controller functions and connect to route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,35 @@
-const authUser = (req, res) => {
-    res.status(200).json({ message: 'Auth User' })
-}
+import asyncHandler from 'express-async-handler'
 
-export { authUser };
+// @desc    Auth user/ set token
+// @route    POST /api/users/auth
+const authUser = asyncHandler(async(req, res) => {
+    res.status(401);
+    throw new Error('Unauthorized')
+    res.status(200).json({ message: 'Auth User' })
+});
+
+// @desc    Logout user
+// @route    POST /api/users/logout
+const logoutUser = asyncHandler(async(req, res)=> {
+    res.status(200).json({ message: 'Logout user'})
+})
+
+// @desc    Register user
+// @route    POST /api/users
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Register user' })
+});
+
+// @desc    Get user profile
+// @route    GET /api/users/profile
+const getUserProfile = asyncHandler(async(req, res) => {
+    res.status(200).json({ message: 'Get user profile' })
+});
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Update user profile'})
+});
+
+export { authUser, logoutUser, registerUser, getUserProfile, updateUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUserProfile, logoutUser, registerUser, updateUserProfile } from '../controllers/userController.js';
 
-router.get('/auth', authUser)
-
+router.post('/', registerUser)
+router.post('/auth', authUser)
+router.post('logout', logoutUser)
+router.get('/profile', getUserProfile)
+router.put('/profile', updateUserProfile)
 export default router;
